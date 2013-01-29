5064b3ff21c42541f5582d78f15f831b80789d50
yipingma <yma@sunrunhome.com>
2013-01-28 16:53:13 -0800

    1/28 o

---
+++ games/src/main/BestTime_Stock3.java
@@ -0,0 +1,25 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ * */
+package main;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BestTime_Stock3 {
+	
+	public static void main(String[] args){
+		Scanner input= new Scanner(System.in);
+		System.out.println("Input Array, split with ',': ");
+		String myinput=input.nextLine();
+		ArrayList<Integer> mylist =new ArrayList();
+		
+	}
+	
+
+}
--- games/src/main/Valid_Palindrome.java
+++ games/src/main/Valid_Palindrome.java
@@ -7,14 +7,49 @@ For example,
  */
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Object;
+
 public class Valid_Palindrome {
+	private static String myinput;
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Scanner**
+		Scanner input=new Scanner(System.in);
+		System.out.println("Input the string:");
+		myinput=input.nextLine();
+		//int number = input.nextInt();
+		//float decimal = input.nextFloat();
 
+		int i, j;
+		//ArrayList<element> array_name=new ArrayList()**
+		ArrayList<String> mylist=new ArrayList();
+		for(i=0; i<myinput.length(); i++){
+			//Character.isLetter** determine if a char is a letter--Character.isDigit(ch), Character.isSpaceChar(ch)
+			if(Character.isLetter(myinput.charAt(i))){
+				//Character.toString()** convert char to string(arraylist.add() only support string as parameters)
+				mylist.add(Character.toString(myinput.charAt(i)));				
+			}
+		}
+		System.out.println(mylist);
+		
+		//size()** for arraylist, length()** is for array
+		for(j=0; j<mylist.size(); j++){
+			//arraylist.add(int index)**,arraylist.get(int index)**
+			//equals()** equalsIgnoreCase()** comparing string should not use ==, which is comparing the reference
+			if(mylist.get(j).equalsIgnoreCase(mylist.get(mylist.size()-1-j))){
+				continue;
+			}else{
+				System.out.println("This is NOT a palindrome");
+				return;
+			}
+		}
+		System.out.println("COOL!!!! This is a palindrome");
 	}
 
 }
